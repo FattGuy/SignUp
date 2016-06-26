5c54cdfbb2dcda2e8d3e93bef99192008ec21712
FattGuy <chang809@umn.edu>
2016-06-26 13:46:05 -0500

    finish UI

---
+++ SignUP/SignUpViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SignUpViewController.swift
+//  SignUP
+//
+//  Created by Feng Chang on 6/26/16.
+//  Copyright © 2016 FattGuy. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var usrnameTextField: UITextField!
+    @IBOutlet weak var pwdTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
